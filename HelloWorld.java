69fde16d1ad5a5d907477f75417666b74b0bfc33
Ignacio Arces <ignacio@ooyala.com>
2015-03-16 21:42:14 -0600

    saludo capitalizado

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
         
         if ( args.length > 0 ) {
             who = args[0];
+            char firstLetter = who.charAt(0);
+            who = Character.toUpperCase(firstLetter) + who.substring(1);
         }
 
         System.out.println(String.format("Hello %s!!!", who)); 
