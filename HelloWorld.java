0b8253b7e81cb2875fbf1499fcc7562aa832df53
Javier Azanza <azzanza09@gmail.com>
2015-03-16 20:39:57 -0600

    se arreglo el error cuando no especifica a quien saludar

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world  " + args[0]);
-    }    
+       if ( args.length > 0 ) {
+        System.out.println("hello " + args[0]);
+       } else{
+
+        System.out.println("hello world");
+       }
+       }    
 }
 
